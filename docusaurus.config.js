9fd6704bf8d5807926846abf7aa925f3b1967461
Nirjal Bhurtel <81609547+erwinschrodinger1@users.noreply.github.com>
2023-08-08 00:42:05 +0545

    Update docusaurus.config.js to ignore on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'kuosc2005', // Usually your GitHub org/user name.
   projectName: 'website', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
