4d9029fc7e099b72596857046a320a84d2ab4a6f
Aadarsha Dhakal <braveboy2058@gmail.com>
2023-08-14 02:48:42 +0545

    Add sponsor section

---
+++ src/components/SponsorAvatar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const SponsorAvatar = ({ name, logo, url }) => {
+  return (
+    <a title={name} target="_blank" href={url} rel="noreferrer">
+      <div className={styles.sponsor_wrapper}>
+        <img className={styles.sponsor_avatar} src={logo} alt={name} />{' '}
+      </div>{' '}
+    </a>
+  );
+};
+
+export default SponsorAvatar;
