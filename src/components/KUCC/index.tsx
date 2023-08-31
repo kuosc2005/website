d52a1ff692e7ce2aacba7098b80221ca2221beeb
Sanjib Dahal <sanjibdahal04@gmail.com>
2023-08-31 22:27:58 +0545

    feat: added headings in About

--- src/components/KUCC/index.tsx
+++ src/components/KUCC/index.tsx
@@ -6,16 +6,12 @@ const KUCC = () => {
 		<>
 			<div className={"container banner"}>
 				<img src={"/img/kucc-logo.png"} alt="KUCC" />
+				<div className={"content"}>
+				<h1>Kathmandu University Computer Club</h1>
 				<p>
-					Kathmandu University Computer Club KUCC is a non-profit, independent
-					club formed by students of the Department of Computer Science and
-					Engineering in the year 1997. Being registered as the first club of
-					Kathmandu University with the registration number 001/1997 in the
-					Student Welfare, KUCC has worked in the field of ICT for twenty years.
-					As an autonomous community supported by the esteemed Kathmandu
-					University Computer Club (KUCC), KUOSC radiates the same spirit of
-					technological excellence and collaboration.
+				KUCC is a non-profit, independent club formed by students of the Department of Computer Science and Engineering in the year 1997. Being registered as the first club of Kathmandu University with the registration number 001/1997 in the Student Welfare, KUCC has worked in the field of ICT for twenty years. KUCC has more than 1000 members from Department of Computer Science and Engineering. Kathmandu University Computer Club (KUCC) is a student wing of the Department of Computer Science and Engineering which was established with a goal to engage and aware students in the technological research and development, most prominently in the ICT field and at the same time provides a common platform for young and aspiring individuals to exhibit their ideas. Every year KUCC cooperates and organizes numerous competitive as well as non-competitive events like seminars, exhibitions, hackathon, skill development program and tutorial sessions to name a few, with an unwavering intention to promote, develop and encourage emerging technological advancement.
 				</p>
+				</div>
 			</div>
 		</>
 	);
--- src/components/KUOSC/index.tsx
+++ src/components/KUOSC/index.tsx
@@ -6,11 +6,14 @@ const KUOSC = () => {
 		<>
 			<div className={"container banner"}>
 				<img src={"/img/kuosc.png"} alt="KUOSC" />
+				<div className={"content"}>
+					<h1>Kathmandu University Open Source Community</h1>
 				<p>
-				Kathmandu University Open Source Community (KUOSC) is the Open Source related wing of KUCC. It is a vibrant historic community from Kathmandu University which provides a common platform for all FOSS enthusiasts to discuss, share, and learn about free and open source softwares.
+				KUOSC is the Open Source related wing of KUCC. It is a vibrant historic community from Kathmandu University which provides a common platform for all FOSS enthusiasts to discuss, share, and learn about free and open source softwares.
 
 				KUOSC is specifically focused on all the Open Source related activities ranging from training programs like Linux Talks, Useful Network Training to advocacy activities like Software Freedom Day celebration and organizing different community level workshops and events.
 				</p>
+				</div>
 			</div>
 		</>
 	);
