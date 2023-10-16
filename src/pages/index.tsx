2253d4d040c29b829931b88537e5baf88bac640f
Sabin <sabinonweb7@gmail.com>
2023-10-16 17:21:22 +0545

    Link error fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
 				<h1 className="hero__title">{siteConfig.title}</h1>
 				<p className="hero__subtitle">{siteConfig.tagline}</p>
 				<div className={styles.buttons}>
-					<Link className="button button--secondary button--lg" to="/docs/">
+					<Link className="button button--secondary button--lg" to="/docs/intro">
 						Know Our Projects
 					</Link>
 					<Link className="button button--secondary button--lg" to="https://www.youtube.com/@kuosc">
